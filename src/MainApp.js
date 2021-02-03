42a91db3b179f83f336c3f7af845187ea1420a84
irvincnt <irvincnt93@gmail.com>
2021-02-03 17:58:30 -0600

    New app - React router

---
+++ src/MainApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainApp = () => {
+  return ( 
+    <div>
+      <h1>Main Page</h1>
+      <hr />
+    </div>
+   );
+}
+ 
+export default MainApp;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MainApp from './MainApp';
+//import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
